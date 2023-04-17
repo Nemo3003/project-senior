fa521f752939746a8cde7d33d08feee035f15b35
Nemo3003 <ulimarian2000@gmail.com>
2023-04-17 15:57:32 -0300

    [UPD] adding information and an image to hero

--- project-senior-byu/src/components/Hero.jsx
+++ project-senior-byu/src/components/Hero.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className=' h-[90vh] grid grid-cols-1 md:grid-cols-8 p-8'>
+      {/*Info*/}
+      <div className=' md:col-span-5 flex items-center justify-center'>
+        <div>
+          <h1 className='text-7xl font-bold'>
+            We help you prepare for your professional 
+            <span className='text-primary p-2 border-8 border-primary relative'>success</span>
+            </h1>
+          <p>Description</p>
+          <button>Button</button>
+        </div>
+      </div>
+      <div className='bg-red-600 md:col-span-3'>Image</div>
+    </section>
   )
 }
 
