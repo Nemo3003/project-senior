89bcd844721a7e1a65716ac5d3827c02af046fb8
Nemo3003 <ulimarian2000@gmail.com>
2023-07-04 10:18:52 -0300

    [UPD] fixing credentials

--- project-senior-byu/root/backend/src/db/db.js
+++ project-senior-byu/root/backend/src/db/db.js
@@ -7,10 +7,12 @@ const {
   DB_PORT
 } = require('./config.js')
 
-export const pool = createPool({
+const pool = createPool({
   user: DB_USER,
   password: DB_PASSWORD,
   host: DB_HOST,
   port: DB_PORT,
   database: DB_NAME
 })
+
+module.exports = pool
