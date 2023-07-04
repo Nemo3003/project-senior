4334c3f0c28ea721b1db441e86dd148388d8cbd5
Nemo3003 <ulimarian2000@gmail.com>
2023-07-04 18:13:31 -0300

    [UPD] setting headers

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -32,7 +32,7 @@ app.use(cors({
 }));
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ocacoplus.onrender.com/*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://heartfelt-twilight-23e637.netlify.app');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   // other headers and configurations
   next();
