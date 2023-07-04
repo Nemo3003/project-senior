2d47fc824a29d72b85c3d36143c80d3212f16a4c
Nemo3003 <ulimarian2000@gmail.com>
2023-07-04 09:10:48 -0300

    [UPD] Trying again

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 dotenv.config();
-const mysql = require('mysql2')
+const mysql = require('mysql2/promise')
 
 const authRoute = require('./src/routes/auth.routes')
 const adminRoute = require('./src/routes/admin.routes')
@@ -34,8 +34,8 @@ app.use(function(req, res, next) {
 
 const port_nd = 8081;
 app.listen(port_nd, ()=>console.log(`Listening on port ${port_nd}`));
-
-const db = mysql.createConnection({
+async function server() {
+const db =  mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
@@ -43,13 +43,19 @@ const db = mysql.createConnection({
   ssl: {
     rejectUnauthorized: true, // Disables SSL/TLS certificate verification
   }
+
+  
 });
-
-const routes = [authRoute,adminRoute,classesRoute,usersRoute,]
-
-app.use('/', ...routes)
 
 db.connect(err => {
   if (err) {console.error(err.message);}
   console.log("Database connected");
 });
+
+(await db).query("SELECT * FROM users")
+}
+
+const routes = [authRoute,adminRoute,classesRoute,usersRoute,]
+
+app.use('/', ...routes)
+
