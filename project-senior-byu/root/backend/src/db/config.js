aa107b18e2358c48e2e8dcf08124db61d7741791
Nemo3003 <ulimarian2000@gmail.com>
2023-07-05 11:29:21 -0300

    [UPD] endpoints being worked on

--- project-senior-byu/root/backend/src/db/config.js
+++ project-senior-byu/root/backend/src/db/config.js
@@ -1,13 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
  
- const PORT = process.env.PORT || 3000
+ const PORT=process.env.PORT || 3000
 
- const DB_HOST = process.env.DB_HOST || 'localhost'
- const DB_USER = process.env.DB_USER || 'root'
- const DB_PASSWORD = process.env.DB_PASSWORD || ''
- const DB_NAME = process.env.DB_NAME || 'ocacoplus'
- const DB_PORT = process.env.DB_PORT || 3306
+ const DB_HOST=process.env.DB_HOST || 'localhost'
+ const DB_USER=process.env.DB_USER || 'root'
+ const DB_PASSWORD=process.env.DB_PASSWORD || ''
+ const DB_NAME=process.env.DB_NAME || 'ocacoplus'
+ const DB_PORT=process.env.DB_PORT || 3306
 
 module.exports = {
     DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, PORT
