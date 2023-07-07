e119114137490ac4a1155098315fdf5e0db10315
Nemo3003 <ulimarian2000@gmail.com>
2023-07-07 10:01:20 -0300

    [UPD] perhaps vercel?

--- project-senior-byu/root/backend/src/db/config.js
+++ project-senior-byu/root/backend/src/db/config.js
@@ -3,11 +3,6 @@ dotenv.config();
  
  const PORT=process.env.PORT || 3000
 
- const DB_HOST=process.env.DB_HOST || 'localhost'
- const DB_USER=process.env.DB_USER || 'root'
- const DB_PASSWORD=process.env.DB_PASSWORD || ''
- const DB_NAME=process.env.DB_NAME || 'ocacoplus'
- const DB_PORT=process.env.DB_PORT || 3306
  const DB_URI = process.env.DB_URL
 
 module.exports = {
