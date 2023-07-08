490906c4ca03eebe7388ac8d0e5a545be6f224cb
Nemo3003 <ulimarian2000@gmail.com>
2023-07-08 09:48:02 -0300

    [UPD] cors enabling

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -22,12 +22,20 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-const corsOptions = {
-  origin: ['https://heartfelt-twilight-23e637.netlify.app', 'https://ocacoplus-server.onrender.com'],
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:8081', 'https://ocacoplus-server.onrender.com', 'https://heartfelt-twilight-23e637.netlify.app'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 const port_nd = 8081;
 app.listen(port_nd, () => console.log(`Listening on port ${port_nd}`));
 
