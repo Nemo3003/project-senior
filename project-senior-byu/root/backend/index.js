5dd91c75ad8198648b8a7f552000346eb8689f12
Nemo3003 <ulimarian2000@gmail.com>
2023-07-08 14:24:14 -0300

    [UPD] again, cors

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -23,8 +23,12 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.header("Access-Control-Allow-Origin", "https://heartfelt-twilight-23e637.netlify.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ 
   res.header('Access-Control-Allow-Credentials', true);
   next();
 });
