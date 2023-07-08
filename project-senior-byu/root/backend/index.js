b27e39f8a8cd47e9dd22ba61f2d49cc2815ea161
Nemo3003 <ulimarian2000@gmail.com>
2023-07-08 14:16:33 -0300

    [UPD] Experimenting with counters

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -23,9 +23,9 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://heartfelt-twilight-23e637.netlify.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header(Access-Control-Allow-Origin, "https://heartfelt-twilight-23e637.netlify.app");
+  res.header(Access-Control-Allow-Headers, "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(Access-Control-Allow-Credentials, true);
   next();
 });
 
