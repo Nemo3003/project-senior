7fdaee4cc2a1fc52faafe28565d9d50bcbd5ba7c
Nemo3003 <ulimarian2000@gmail.com>
2023-07-08 14:35:00 -0300

    [UPD] I give up

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -23,10 +23,9 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use(function(req, res, next) {
-        res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', "https://heartfelt-twilight-23e637.netlify.app")
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  res.header("Access-Control-Allow-Origin", "https://heartfelt-twilight-23e637.netlify.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
