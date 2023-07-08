ce6e5d281dbcea110b266ccd35008b1b5a4f1a49
Nemo3003 <ulimarian2000@gmail.com>
2023-07-08 11:15:27 -0300

    [UPD] trying to make cors work

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -22,10 +22,18 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-app.use(cors({
-  origin: "https://heartfelt-twilight-23e637.netlify.app",
-  credentials: true
-}))
+const corsOptions = {
+  origin: 'https://heartfelt-twilight-23e637.netlify.app',
+  credentials: true,
+};
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', corsOptions.origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+app.use(cors(corsOptions));
 
 const port_nd = 8081;
 app.listen(port_nd, () => console.log(`Listening on port ${port_nd}`));
