09c17f81de6f10ebd06b8c1ec8b6b101c89695fa
Nemo3003 <ulimarian2000@gmail.com>
2023-07-08 09:42:17 -0300

    [UPD] cors enabling

--- project-senior-byu/root/backend/index.js
+++ project-senior-byu/root/backend/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 const corsOptions = {
-  origin: 'https://heartfelt-twilight-23e637.netlify.app',
+  origin: '*',
   credentials: true,
 };
 
